79e84fe8f80f6eee100580504df9ae689d87fc46
Duyen Le Huong <128472122+rayale14@users.noreply.github.com>
2023-07-26 16:46:28 +0700

    Update schema-sql.sql

--- Case Study #2 - Pizza Runner/schema-sql.sql
+++ Case Study #2 - Pizza Runner/schema-sql.sql
@@ -3,11 +3,11 @@ SET search_path = pizza_runner;
 
 DROP TABLE IF EXISTS runners;
 CREATE TABLE runners (
-  "runner_id" INTEGER,
-  "registration_date" DATE
+  runner_id INTEGER,
+  registration_date DATE
 );
 INSERT INTO runners
-  ("runner_id", "registration_date")
+  (runner_id, registration_date)
 VALUES
   (1, '2021-01-01'),
   (2, '2021-01-03'),
